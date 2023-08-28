09930f3c7b930229e88a00636cf3674bd1400ea3
ztekstorm <ztek975@gmail.com>
2023-08-28 13:22:25 +0200

    Removed script

--- target/classes/navigation.js
+++
@@ -1,8 +0,0 @@
-function moveToArea(level) {
-  var area = document.getElementById('area-selector').value;
-  var path = '';
-  for (let i = 0; i < level; i++) {
-    path += '../';
-  }
-  window.location = path + area + "/index.html";
-}
